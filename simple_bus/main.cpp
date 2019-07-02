c6a94f53a50357ca3147f5654d2414be5f2e159c
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2019-07-02 11:41:18 -0300

    Added simple bus

---
+++ simple_bus/main.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+int sc_main (int argc, char * argv[]) {
+    // insert code here...
+    std::cout << "Hello SystemC!\n";
+    sc_start();
+    return 0;
+}
---
+++ simple_bus/pch.h
@@ -0,0 +1,10 @@
+#ifndef PCH_H
+#define PCH_H
+
+// SystemC
+#include "systemc.h"
+
+// C++ Standard Library
+#include <iostream>
+
+#endif //PCH_H
